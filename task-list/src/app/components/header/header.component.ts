c5069f3c91c4af8964aa42a42ebbb57e48d2e17a
cristhianocania <cris.ocania@gmail.com>
2022-05-12 19:14:10 -0300

    mock de tareas!

--- task-list/src/app/components/header/header.component.ts
+++ task-list/src/app/components/header/header.component.ts
@@ -12,7 +12,7 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  toogleAndTask(){
+  toggleAddTask(){
 
     console.log("asdasd");
   }
---
+++ task-list/src/app/mock-tasks.ts
@@ -0,0 +1,30 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[]= [
+    {
+    id:1,
+    text:"Terminar nuestro primer modulo de angular",
+    day:'agosto 5 a las 12:00',
+    reminder:true
+    },
+    {
+        id:2,
+        text:"hacer las compras",
+        day:'agosto 5 a las 12:00',
+        reminder:true
+    },
+    {
+        id:3,
+        text:"Investigar sobre boottrap",
+        day:'agosto 5 a las 12:00',
+        reminder:false
+    },
+    {
+        id:4 ,
+        text:"bañar a mi bebe",
+        day:'agosto 5 a las 12:00',
+        reminder:false
+    }
+
+    
+]
